063030d13a5bac46ceef67ace4e45491fc0e869b
Bram de Leeuw <contact@bram-de-leeuw.nl>
2015-03-29 00:20:20 +0100

    added a regex that dissects the log file to JSON that i can parse in jade

--- index.js
+++ index.js
@@ -27,18 +27,88 @@ app.get('/print/:logfile', function( request, response ) {
   var renderData = {};
   var logfile = request.params.logfile;
   var filePath = 'logs/'+logfile;
+  var file = fs.createReadStream(filePath);
+  var remaining = '';
+  var dissectedData = [];
+  renderData.title = logfile;
 
-  renderData.title = 'OneMinute '+logfile;
 
-  fs.readFile( filePath , 'utf8', function ( err, file ) {
-    if (err) { throw err; }
+  renderData.logData = readLines(file);
 
-    renderData.file = file;
+  file.on('data', function(data) {
+    remaining += data;
+    var index = remaining.indexOf('\n');
+    while (index > -1) {
+      var line = remaining.substring(0, index);
+      remaining = remaining.substring(index + 1);
+      dissectedData.push(dissect(line));
+      index = remaining.indexOf('\n');
+    }
+  });
+
+  file.on('end', function() {
+    if (remaining.length > 0) {
+      dissectedData.push(dissect(remaining));
+    }
+
+    renderData.logData = dissectedData;
     response.render('booklet', renderData);
-
   });
 });
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
+
+// functions
+function readLines(input) {
+  var remaining = '';
+  var dissectedData = [];
+
+  input.on('data', function(data) {
+    remaining += data;
+    var index = remaining.indexOf('\n');
+    while (index > -1) {
+      var line = remaining.substring(0, index);
+      remaining = remaining.substring(index + 1);
+      dissectedData.push(dissect(line));
+      index = remaining.indexOf('\n');
+    }
+  });
+
+  input.on('end', function() {
+    if (remaining.length > 0) {
+      dissectedData.push(dissect(remaining));
+    }
+  });
+}
+
+function dissect(line) {
+
+    var regex = /(\S+?) (\S+?) (\S+?) (\S+? \+\S+?\]) "(\S+?) (\S+?) (\S+?)" (\S+?) (\S+?) "(\S+?)" "(.*?)"/
+    var map = {
+      1: 'deviceName',
+      2: 'unknown',
+      3: 'unknownTwo',
+      4: 'timestamp',
+      5: 'getpost?',
+      6: 'url',
+      7: 'protocol',
+      8: 'unknownThree',
+      9: 'unknownFour',
+      10: 'unknownFive',
+      11: 'deviceType'
+    }
+
+    var matches = line.match(regex)
+                , ret = {};
+
+    if (!matches) return null;
+
+    for (var k in map) {
+      var v = map[k];
+      ret[v] = matches[k];
+    }
+
+    return ret;
+  }
